1bcb21b569714dbe175ba449c6fe2bac42a5952e
Misherz <michelle.j.lugo@gmail.com>
2024-11-17 16:05:37 -0500

    Home/About page base nearly completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import './css/body.css'
 import HomePage from './pages/homePage/HomePage'
 
 function App() {
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,10 +1,14 @@
 import NavBar from "../../components/navBar/NavBar"
+import AboutPage from "./AboutPage"
+import '../../css/homePg.css'
 
 
 export default function HomePage(){
     return (
         <div>
             <NavBar />
+            <h2 className="homeHeader">Welcome to Happy Tails Notebooks</h2>
+            <img src="/imgs/FoxLogo.png" alt="Image of Happy Tails Notebooks Fox mascot" className="foxImg" />
         </div>
     )
 }
