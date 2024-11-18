537933be9341dadb288ae14b04127c2d5a9b8936
Misherz <michelle.j.lugo@gmail.com>
2024-11-17 22:42:30 -0500

    dotted notebook pattern in background

---
+++ src/pages/homePage/BackgroundImg.jsx
@@ -0,0 +1,8 @@
+import '../../../public/imgs/gridPaper.png'
+import '../../css/body.css'
+
+export default function BackgroundImg(){
+    return (
+        <img src="./imgs/gridPaper.png" alt='dotted notebook background' className='bkgDots'/>
+    )
+}
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,11 +1,19 @@
 import NavBar from "../../components/navBar/NavBar"
 import AboutPage from "./AboutPage"
 import '../../css/homePg.css'
+import { useNavigate, Link } from "react-router-dom"
+import { useState } from "react";
+import BackgroundImg from "./BackgroundImg";
 
 
 export default function HomePage(){
+    function handleOnClick(){};
+    function handleChange(){};
+    
+
     return (
         <div>
+            <BackgroundImg />
             <NavBar />
             <h2 className="homeHeader">Welcome to Happy Tails Notebooks</h2>
             <img src="/imgs/FoxLogo.png" alt="Image of Happy Tails Notebooks Fox mascot" className="foxImg" />
